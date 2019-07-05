f151f76b7c7e72a238ba5a4fcb56ced8a085134a
Andre JS <andrej.silva@outlook.com>
2019-07-05 15:58:03 -0300

    Adicionado o controlador de Fabricantes. Pequenas alterações na configuração de conexão com o Banco

---
+++ ProjetoLivroASPMVC/Controllers/FabricantesController.cs
@@ -0,0 +1,19 @@
+﻿using ProjetoLivroASPMVC.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoLivroASPMVC.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        private EFContext context = new EFContext();
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View(context.Fabricantes.OrderBy(f => f.Nome));
+        }
+    }
+}
