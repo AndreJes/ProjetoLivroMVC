212bda92938624bbc7e45951cdd0581e1bb57a70
Andre JS <andrej.silva@outlook.com>
2019-07-05 15:46:07 -0300

    Adicionado contexto do banco de dados e configurada a ConnectionString

---
+++ ProjetoLivroASPMVC/Contexts/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using ProjetoLivroASPMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLivroASPMVC.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+    }
+}
