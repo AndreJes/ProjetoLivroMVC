30f9af6b917fcc7463539ed7a8e9270d8530fd42
Andre JS <andrej.silva@outlook.com>
2019-08-11 11:06:51 -0300

    Iniciada a Implementação do OWIN e Sistema de Login

---
+++ ProjetoLivroASPMVC/App_Start/IdentityConfig.cs
@@ -0,0 +1,23 @@
+﻿using Owin;
+using ProjetoLivroASPMVC.DAL;
+using ProjetoLivroASPMVC.Infraestrutura;
+using Microsoft.Owin.Security.Cookies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+
+namespace ProjetoLivroASPMVC
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<IdentityDbContextAplicacao>(IdentityDbContextAplicacao.Create);
+            app.CreatePerOwinContext<GerenciadorUsuario>(GerenciadorUsuario.Create);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie, LoginPath = new PathString("/Seguranca/Account/Login"));
+        }
+    }
+}
---
+++ ProjetoLivroASPMVC/DAL/IdentityDbContextAplicacao.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using ProjetoLivroASPMVC.Areas.Seguranca.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoLivroASPMVC.DAL
+{
+    public class IdentityDbContextAplicacao : IdentityDbContext<Usuario>
+    {
+        public IdentityDbContextAplicacao() : base("MySQL_UserDB") { }
+
+        static IdentityDbContextAplicacao()
+        {
+            Database.SetInitializer<IdentityDbContextAplicacao>(new IdentityDbInit());
+        }
+
+        public static IdentityDbContextAplicacao Create()
+        {
+            return new IdentityDbContextAplicacao();
+        }
+    }
+
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<IdentityDbContextAplicacao> { }
+}
